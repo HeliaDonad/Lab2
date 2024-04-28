4314828c3ed9bf6c9f0befec030255424602e2cd
HeliaDonad <r0936750@student.thomasmore.be>
2024-04-28 14:48:52 +0200

    nav

--- index.php
+++ index.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>federaalinstituutmensenrechten</title>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
-    
+<header class="header">
+    <div class="logo">
+        <img src="images/logo2.svg" alt="Logo">
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a href="#">Mijn mensenrechten</a></li>
+            <li><a href="#">Thema's</a></li>
+            <li><a href="#">Zittingen</a></li>
+            <li><a href="#">Vrijwilligerswerk</a></li>
+            <li><a href="#">Activiteiten</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="actions">
+    <div class="action-icon"><i class="material-icons">notifications</i></div>
+    <div class="action-icon"><i class="material-icons">settings</i></div>
+    <div class="action-icon"><i class="material-icons">account_circle</i></div>
+</div>
+</header>
 </body>
 </html>
