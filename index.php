a29b9aa39ac57dee6cb23e77508bc7d6ba17d2ca
HeliaDonad <r0936750@student.thomasmore.be>
2024-04-30 23:51:11 +0200

    wegwijzer dashboard mobiel

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>federaalinstituutmensenrechten</title>
     <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
@@ -32,5 +33,74 @@
 </div>
 </header>
     <a href="/LAB2/signin_login_logout/logout.php">Log Out</a>
+
+    <div class="container">
+    <div class="button-bar">
+        <div class="button wegwijzer">
+            <span class="text">Mijn wegwijzer</span>
+            <img src="images/iconen/mijnwegwijzer.svg" alt="Mijn wegwijzer">
+        </div>
+        <div class="button">
+            <span class="text">Discriminatie</span>
+            <img src="images/iconen/discriminatie.svg" alt="Discriminatie">
+        </div>
+        <div class="button">
+            <span class="text">Persoongegevens</span>
+            <img src="images/iconen/persoongegevens.svg" alt="Persoongegevens">
+        </div>
+        <div class="button">
+            <span class="text">Kinderrechten</span>
+            <img src="images/iconen/kinderrechten.svg" alt="Kinderrechten">
+        </div>
+        <div class="button">
+            <span class="text">Publieke en private ombudsmannen</span>
+            <img src="images/iconen/publieke_en_private_ombudsmannen.svg" alt="Publieke en private ombudsmannen">
+        </div>
+        <div class="button">
+            <span class="text">Migranten</span>
+            <img src="images/iconen/migranten.svg" alt="Migranten">
+        </div>
+        <div class="button">
+            <span class="text">Gevangenis</span>
+            <img src="images/iconen/gevangenis.svg" alt="Gevangenis">
+        </div>
+        <div class="button">
+            <span class="text">Politie</span>
+            <img src="images/iconen/politie.svg" alt="Politie">
+        </div>
+        <div class="button">
+            <span class="text">Mindervaliden</span>
+            <img src="images/iconen/mindervaliden.svg" alt="Mindervaliden">
+        </div>
+        <div class="button">
+            <span class="text">Ouderen</span>
+            <img src="images/iconen/ouderen.svg" alt="Ouderen">
+        </div>
+        <div class="button">
+            <span class="text">Gendergelijkheid</span>
+            <img src="images/iconen/gendergelijkheid.svg" alt="Gendergelijkheid">
+        </div>
+        <div class="button">
+            <span class="text">Armoede</span>
+            <img src="images/iconen/armoede.svg" alt="Armoede">
+        </div>
+        <div class="button">
+            <span class="text">Wonen</span>
+            <img src="images/iconen/wonen.svg" alt="Wonen">
+        </div>
+        <div class="button">
+            <span class="text">LGBTI+</span>
+            <img src="images/iconen/lgbti+.svg" alt="LGBTI+">
+        </div>
+        <div class="button">
+            <span class="text">Daklozen</span>
+            <img src="images/iconen/daklozen.svg" alt="Daklozen">
+        </div>
+        <div class="button">
+            <span class="text">Frans-Duitse gemeenschap</span>
+            <img src="images/iconen/frans_duitse_gemeenschap.svg" alt="Frans-Duitse gemeenschap">
+        </div>
+    </div>
+</div>
 </body>
 </html>
