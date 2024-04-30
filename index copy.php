36ddebf36b9d6c15ba8ea44222e8e171044e228b
HeliaDonad <r0936750@student.thomasmore.be>
2024-04-30 20:19:15 +0200

    navigatie + hamburger

--- index copy.php
+++ index copy.php
@@ -4,26 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>federaalinstituutmensenrechten</title>
-    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
 <header class="header">
-<nav>
-    <label class="burger" for="burger">&#9776;</label>
-    <input type="checkbox" id="burger">
-
-    <a class="logo" href="#">
-        <img src="images/logo2.svg" alt="logo">
-    </a>
-    <ul class="subnav">
-        <li><a href="#">Mijn mensenrechten</a></li>
-        <li><a href="#">Thema's</a></li>
-        <li><a href="#">Zittingen</a></li>
-        <li><a href="#">Vrijwilligerswerk</a></li>
-        <li><a href="#">Activiteiten</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
+    <div class="logo">
+        <img src="images/logo2.svg" alt="Logo">
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a href="#">Mijn mensenrechten</a></li>
+            <li><a href="#">Thema's</a></li>
+            <li><a href="#">Zittingen</a></li>
+            <li><a href="#">Vrijwilligerswerk</a></li>
+            <li><a href="#">Activiteiten</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     </nav>
     <div class="actions">
     <div class="action-icon"><i class="material-icons">notifications</i></div>
