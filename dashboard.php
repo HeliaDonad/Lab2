709109ee262726cc2c0fea1a80b14fc740e1b516
HeliaDonad <r0936750@student.thomasmore.be>
2024-05-29 16:17:01 +0200

    orde css nog

---
+++ dashboard.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>federaalinstituutmensenrechten</title>
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+<header class="header">
+<nav>
+    <label class="burger" for="burger">&#9776;</label>
+    <input type="checkbox" id="burger">
+
+    <a class="logo" href="#">
+        <img src="images/general/logo.svg" alt="logo">
+    </a>
+    <ul class="subnav">
+        <li><a href="#">Mijn mensenrechten</a></li>
+        <li><a href="#">Thema's</a></li>
+        <li><a href="#">Zittingen</a></li>
+        <li><a href="#">Vrijwilligerswerk</a></li>
+        <li><a href="#">Activiteiten</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    </nav>
+    <div class="actions">
+    <div class="action-icon"><i class="material-icons">notifications</i></div>
+    <div class="action-icon"><i class="material-icons">settings</i></div>
+    <div class="action-icon"><i class="material-icons">account_circle</i></div>
+</div>
+</header>
+    <a href="/LAB2/signin_login_logout/logout.php">Log Out</a>
+
+    <h1>Mensenrechten</h1>
+    <div class="container">
+    <div class="button-bar">
+        <div class="button wegwijzer">
+            <span class="text">Mijn wegwijzer</span>
+            <img src="images/iconen/mijnwegwijzer.svg" alt="Mijn wegwijzer">
+        </div>
+        <div class="button">
+            <span class="text">Discriminatie</span>
+            <img src="images/iconen/discriminatie.svg" alt="Discriminatie">
+        </div>
+        <div class="button">
+            <span class="text">Persoongegevens</span>
+            <img src="images/iconen/persoongegevens.svg" alt="Persoongegevens">
+        </div>
+        <div class="button">
+            <span class="text">Kinderrechten</span>
+            <img src="images/iconen/kinderrechten.svg" alt="Kinderrechten">
+        </div>
+        <div class="button">
+            <span class="text">Publieke en private ombudsmannen</span>
+            <img src="images/iconen/publieke_en_private_ombudsmannen.svg" alt="Publieke en private ombudsmannen">
+        </div>
+        <div class="button">
+            <span class="text">Migranten</span>
+            <img src="images/iconen/migranten.svg" alt="Migranten">
+        </div>
+        <div class="button">
+            <span class="text">Gevangenis</span>
+            <img src="images/iconen/gevangenis.svg" alt="Gevangenis">
+        </div>
+        <div class="button">
+            <span class="text">Politie</span>
+            <img src="images/iconen/politie.svg" alt="Politie">
+        </div>
+        <div class="button">
+            <span class="text">Mindervaliden</span>
+            <img src="images/iconen/mindervaliden.svg" alt="Mindervaliden">
+        </div>
+        <div class="button">
+            <span class="text">Ouderen</span>
+            <img src="images/iconen/ouderen.svg" alt="Ouderen">
+        </div>
+        <div class="button">
+            <span class="text">Gendergelijkheid</span>
+            <img src="images/iconen/gendergelijkheid.svg" alt="Gendergelijkheid">
+        </div>
+        <div class="button">
+            <span class="text">Armoede</span>
+            <img src="images/iconen/armoede.svg" alt="Armoede">
+        </div>
+        <div class="button">
+            <span class="text">Wonen</span>
+            <img src="images/iconen/wonen.svg" alt="Wonen">
+        </div>
+        <div class="button">
+            <span class="text">LGBTI+</span>
+            <img src="images/iconen/lgbti+.svg" alt="LGBTI+">
+        </div>
+        <div class="button">
+            <span class="text">Daklozen</span>
+            <img src="images/iconen/daklozen.svg" alt="Daklozen">
+        </div>
+        <div class="button">
+            <span class="text">Frans-Duitse gemeenschap</span>
+            <img src="images/iconen/frans_duitse_gemeenschap.svg" alt="Frans-Duitse gemeenschap">
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ signin_login_logout/signin.php
@@ -0,0 +1,72 @@
+<?php 
+include_once(__DIR__ . DIRECTORY_SEPARATOR . "../classes/Db.php");
+
+if(!empty($_POST)){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password']; 
+
+    if ($password !== $confirmPassword) {
+        $error = "Wachtwoorden komen niet overeen";
+    } else {
+        // e-mailadres geldig?
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Als het e-mailadres niet geldig is, foutmelding 
+            $error = "Ongeldig e-mailadres";
+        } else {
+            $options = [
+                'cost' => 15
+            ];
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT, $options);
+            $conn = Db::getConnection();
+            $query = "INSERT INTO users (email, password) VALUES ('$email', '$passwordHash')";
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $result = $conn->query($query);
+            header("Location: ../dashboard.php");
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/signin_login.css">
+</head>
+<body>
+<div class="container">
+    <main>
+        <form action="" method="post">
+            <h2>Maak een account aan.</h2>
+            <?php if(isset($error)): ?>
+            <div class="form__error">
+                <p><?php echo $error; ?></p>
+            </div>
+            <?php endif; ?>
+            <div class="form__field">
+                <label for="Email">Email</label>
+                <input type="text" name="email">
+            </div>
+            <div class="form__field">
+                <label for="Password">Wachtwoord</label>
+                <input type="password" name="password">
+            </div>
+            <div class="form__field">
+                <label for="ConfirmPassword">Bevestig wachtwoord</label>
+                <input type="password" name="confirm_password"> 
+            </div>
+
+            <div class="form__field">
+                <input type="submit" value="Aanmelden" class="btn btn--primary">   
+            </div>
+            <a href="login.php">Ik heb al een account</a>
+        </form>
+    </main>  
+    <div class="image">
+            <img src="../images/login_signin/afbeelding1.svg" alt="login_signin/afbeelding">
+    </div>
+</body>
+</html>
