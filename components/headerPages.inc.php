75a11140a9da0afcd8e7750977ccd122d9820fb8
HeliaDonad <r0936750@student.thomasmore.be>
2024-06-05 19:00:24 +0200

    commit

--- components/headerPages.inc.php
+++ components/headerPages.inc.php
@@ -1,13 +1,13 @@
 <header>
 <nav>
-    <a class="logo" href="#">
+    <a class="logo" href="../dashboard.php">
         <img src="../images/general/logo.svg" alt="logo">
     </a>
     <label class="burger" for="burger">&#9776;</label>
     <label class="close" for="burger">&times;</label>
     <input type="checkbox" id="burger">
     <ul class="subnav">
-        <li><a href="#">Mijn mensenrechten</a></li>
+        <li><a href="../dashboard.php">Mijn mensenrechten</a></li>
         <li><a href="#">Thema's</a></li>
         <li><a href="#">Zittingen</a></li>
         <li><a href="#">Vrijwilligerswerk</a></li>
