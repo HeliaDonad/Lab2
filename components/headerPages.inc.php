c00393874e44e7660d03c0cc88fcb88cb7605d1c
Sedaezt <sedaezzat@icloud.com>
2024-06-13 22:58:04 +0200

    navigatie

--- components/headerPages.inc.php
+++ components/headerPages.inc.php
@@ -12,35 +12,67 @@ $currentFile = basename($_SERVER['SCRIPT_FILENAME'], '.php');
     <label class="close" for="burger">&times;</label>
     <input type="checkbox" id="burger">
     <ul class="subnav">
-        <li <?php if (strpos($_SERVER['REQUEST_URI'], 'dashboard.php') !== false) echo 'class="active"'; ?>>
-            <a href="../dashboard.php">Mijn mensenrechten</a>
+    <li <?php if (strpos($_SERVER['REQUEST_URI'], 'dashboard.php') !== false) echo 'class="active"'; ?>>
+            <a href="../dashboard.php">
+                <img src="../images/iconen/mijn-mensenrechten.svg" alt="Mijn mensenrechten" class="nav-icon" id="icoon-display"> Mijn mensenrechten
+            </a>
         </li>
         <li <?php if (strpos($_SERVER['REQUEST_URI'], 'thema2.php') !== false) echo 'class="active"'; ?>>
-            <a href="../pages/thema2.php">Thema's</a>
+            <a href="../pages/thema2.php">
+                <img src="../images/iconen/thema's.svg" alt="Thema's" class="nav-icon" id="icoon-display"> Thema's
+            </a>
         </li>
         <li <?php if (strpos($_SERVER['REQUEST_URI'], 'zittingen.php') !== false) echo 'class="active"'; ?>>
-            <a href="../pages/zittingen.php">Zittingen</a>
+            <a href="../pages/zittingen.php">
+                <img src="../images/iconen/zittingen.svg" alt="Zittingen" class="nav-icon" id="icoon-display"> Zittingen
+            </a>
         </li>
         <li <?php if (strpos($_SERVER['REQUEST_URI'], 'vrijwilligerswerk.php') !== false) echo 'class="active"'; ?>>
-            <a href="../pages/vrijwilligerswerk.php">Vrijwilligerswerk</a>
+            <a href="../pages/vrijwilligerswerk.php">
+                <img src="../images/iconen/vrijwilligerswerk.svg" alt="Vrijwilligerswerk" class="nav-icon" id="icoon-display"> Vrijwilligerswerk
+            </a>
         </li>
         <li <?php if (strpos($_SERVER['REQUEST_URI'], 'activiteiten.php') !== false) echo 'class="active"'; ?>>
-            <a href="../pages/activiteiten.php">Activiteiten</a>
+            <a href="../pages/activiteiten.php">
+                <img src="../images/iconen/activiteiten.svg" alt="Activiteiten" class="nav-icon" id="icoon-display"> Activiteiten
+            </a>
         </li>
-        <li><a href="#">Contact</a></li>
+        <li>
+            <a href="#">
+                <img src="../images/iconen/contact.svg" alt="Contact" class="nav-icon" id="icoon-display"> Contact
+            </a>
+        </li>
+        <li>
+            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
+                <a href="#"  id="icoon-display">
+                <img src="../images/iconen/notification.svg" alt="notificatie" class="nav-icon" > Notificatie
+                </a>
+                </li>
+                <li>
+                    <a href="#" id="icoon-display">
+                        <img src="../images/iconen/profile.svg" alt="Profiel" class="nav-icon" > Account
+                        </a>
+                        </li>
+            <li class="logout-mobile" id="logout">
+                <a href="../signin_login_logout/logout.php">
+                <a id="logout-mobile" class="logout-button" href="./signin_login_logout/logout.php">Uitloggen</a>
+                </a>
+            </li>
         
-    </ul>
+        <?php endif; ?>
+        </ul>
 </nav>
 <div class="actions">
     <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
-        <div class="action-icon"><i class="material-icons">notifications</i></div>
+        <div id="icon_mobile" class="action-icon"><i class="material-icons">notifications</i></div>
         <!--<div class="action-icon"><i class="material-icons">settings</i></div>-->
-        <div class="action-icon"><i class="material-icons">account_circle</i></div>
-        <a class="logout-button" href="../signin_login_logout/logout.php">Log Out</a>
+        <div id="icon_mobile"  class="action-icon"><i class="material-icons">account_circle</i></div>
+        <a class="logout-button" id="display-logout" href="./signin_login_logout/logout.php">Uitloggen</a>
     <?php else: ?>
-        <a class="login-button" href="../signin_login_logout/login.php">Log In</a>
-        <a class="signin-button" href="../signin_login_logout/signin.php">Aanmelden</a>
+        <a class="login-button" href="./signin_login_logout/login.php">Inloggen</a>
+        <a class="signin-button" href="./signin_login_logout/signin.php">Registreren</a>
     <?php endif; ?>
 </div>
 </header>
 <script src="./navigation.js"></script>
+
