356a9dfced195fd26cac8ba6ba802ed54c7fe500
Sedaezt <sedaezzat@icloud.com>
2024-06-14 11:31:24 +0200

    nav

--- dashboard.php
+++ dashboard.php
@@ -57,9 +57,9 @@ if ($selectedFilterId === null || $selectedFilterId === '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eerste Hulp Bij Mensenrechten</title>
-    <link rel="stylesheet" href="css/nav.css?18938">
-    <link rel="stylesheet" href="css/dashboard.css?65813">
-    <link rel="stylesheet" href="css/shared.css?18385">
+    <link rel="stylesheet" href="css/nav.css?18934">
+    <link rel="stylesheet" href="css/dashboard.css?65814">
+    <link rel="stylesheet" href="css/shared.css?18382">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
