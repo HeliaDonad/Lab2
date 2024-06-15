b9b24d19d693b68a63b6c30accbe56b292b9229f
Sedaezt <sedaezzat@icloud.com>
2024-06-15 23:26:02 +0200

    voorwaarden

---
+++ pages/voorwaarden.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meldingen</title>
+    <link rel="stylesheet" href="../css/nav.css?80970">
+    <link rel="stylesheet" href="../css/shared.css">
+    <link rel="stylesheet" href="../css/voorwaarden.css?02566">
+
+
+</head>
+<body>
+    <?php include_once("../components/headerPages.inc.php"); ?>
+
+
+    <div class="voorwaarden">
+        <h1>Voorwaarden</h1>
+    </div>
+
+    <div class="block">
+        <h4 class="txt">Laatst bijgewerkt op 14/06/2024</h4>
+        <p>Welkom bij het EHBM platform. Door gebruik te maken van het EHBM platform gaat u akkoord met de volgende Algemene Voorwaarden. Lees deze voorwaarden zorgvuldig door voordat u de app gebruikt.</p>
+
+    <div class="txt_two">
+        <h4>1. Gebruikersrechten</h4>
+        <p>1.1 Toegang tot de Diensten: EHBM verleent u het recht om de Diensten te gebruiken in overeenstemming met deze Voorwaarden.</p>
+        <p>1.2 Leeftijdsbeperking: U moet minimaal 16 jaar oud zijn om de Diensten te gebruiken. Door de Diensten te gebruiken, verklaart en garandeert u dat u de vereiste leeftijd heeft.</p>    
+    </div>
+
+    <div class="txt_two">
+        <h4>2. Accountregistratie</h4>
+        <p>2.1 Accountgegevens: U bent verantwoordelijk voor het verstrekken van accurate en actuele informatie bij het registreren voor een account bij EHBM.
+        <p>2.2 Accountbeveiliging: U bent verantwoordelijk voor het handhaven van de vertrouwelijkheid van uw accountinformatie en voor alle activiteiten die plaatsvinden onder uw account.</p>
+    </div>
+
+    <div class="txt_two">
+        <h4>3. Gebruiksbeperkingen</h4>
+        <p>3.1 Verboden gedrag: U stemt ermee in de Diensten niet te gebruiken voor illegale of ongeoorloofde doeleinden. Het is verboden om de Diensten te gebruiken op een manier die schade kan toebrengen aan de app, andere gebruikers of derden.</p>    
+        <p>3.2 Intellectueel eigendom: Alle rechten op de inhoud en functionaliteit van de app zijn eigendom van EHBM. U mag de app niet reproduceren, wijzigen, distribueren, verkopen of anderszins exploiteren zonder onze uitdrukkelijke toestemming.</p>
+    </div>
+
+    <div class="txt_two">
+        <h4>4. Privacy</h4>
+        <p>4.1 Privacybeleid: U stemt in met het privacybeleid van EHBM, dat beschikbaar is op [link naar privacybeleid], en begrijpt hoe uw informatie wordt verzameld, gebruikt en gedeeld.</p>
+    </div>
+    
+
+    <div class="txt_two">
+        <h4>5. Beëndiging</h4>
+        <p>5.1 Beëindiging door EHBM: Wij behouden ons het recht voor om uw toegang tot de Diensten op elk moment en om welke reden dan ook te beëindigen.</p>
+        <p>5.2 Beëindiging door gebruiker: U kunt uw account op elk moment sluiten door contact met ons op te nemen via EHBM@gmail.com.</p>
+    </div>
+
+    <div class="txt_two">
+        <h4>6. Wijzigingen in de Voorwaarden</h4>
+        <p>
+            6.1 Bijwerkingen: Hoppin behoudt zich het recht voor om deze Voorwaarden op elk moment bij te werken. Wijzigingen worden van kracht zodra ze op de app zijn geplaatst. Het is uw verantwoordelijkheid om regelmatig te controleren op updates.
+            Door het EHBM platform te gebruiken, stemt u in met deze Voorwaarden. Als u niet akkoord gaat met deze Voorwaarden, gebruik het platform dan niet.
+            Vragen over deze Voorwaarden kunnen worden gestuurd naar EHBM@gmail.com.
+        </p>
+        <h4 class="last_txt">Dank u voor het gebruik maken van de EHBM!</h4>
+    </div>
+
+
+</body>
+</html>
