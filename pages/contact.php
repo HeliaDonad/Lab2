9f3301636c75049b216339e8d5b5e1f429c846a8
HeliaDonad <r0936750@student.thomasmore.be>
2024-06-15 19:00:29 +0200

    contact

--- pages/contact.php
+++ pages/contact.php
@@ -3,12 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Account</title>
     <link rel="stylesheet" href="../css/nav.css?80970">
-    <link rel="stylesheet" href="../css/contact.css?06978">
+    <link rel="stylesheet" href="../css/contact.css?54978">
     <link rel="stylesheet" href="../css/shared.css">
 </head>
 <body>
     <?php include_once("../components/headerPages.inc.php"); ?>
+    
+    <div class="container">
+        <div class="column">
+            <div class="info-item">
+                <h3>Algemene vragen</h3>
+                <div class="info-text">
+                    <img src="../images/iconen/locatie.svg" alt="Locatie icoon">
+                    <p>Eerste hulp bij mensenrechten<br>Leuvenseweg 48<br>1000 Brussel</p>
+                </div>
+            </div>
+            <div class="info-item">
+                <div class="info-text">
+                    <img src="../images/iconen/mail.svg" alt="Mail icoon">
+                    <p><a href="mailto:info@ehbm-psdh.be">info@ehbm-psdh.be</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="info-item pers">
+                <h3>Persvragen</h3>
+                <div class="info-text">
+                    <img src="../images/iconen/mail.svg" alt="Mail icoon">
+                    <p>Seda Ezzat<br><a href="mailto:seda.ezzat@ehbm-psdh.be">seda.ezzat@ehbm-psdh.be</a></p>
+                </div>
+                <div class="info-text">
+                    <img src="../images/iconen/contact_icon.svg" alt="Telefoon icoon">
+                    <p><a href="tel:+324758823">04 75 88 23</a></p>
+                </div>
+                <div class="info-text">
+                    <img src="../images/iconen/mail.svg" alt="Mail icoon">
+                    <p>Helia Donadello<br><a href="mailto:helia.donadello@ehbm-psdh.be">helia.donadello@ehbm-psdh.be</a></p>
+                </div>
+                <div style="margin-bottom: 100px;" class="info-text">
+                    <img src="../images/iconen/contact_icon.svg" alt="Telefoon icoon">
+                    <p><a href="tel:+32475981624">04 75 98 16 24</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
