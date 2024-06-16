a17c265faf214dee92c1a2df0ec267637d41cc05
HeliaDonad <r0936750@student.thomasmore.be>
2024-06-16 11:39:53 +0200

    css aanpassingen

--- pages/account.php
+++ pages/account.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account</title>
     <link rel="stylesheet" href="../css/nav.css?80970">
-    <link rel="stylesheet" href="../css/account.css?06978">
+    <link rel="stylesheet" href="../css/account.css?86978">
     <link rel="stylesheet" href="../css/shared.css">
 </head>
 <body>
--- pages/meldingen.php
+++ pages/meldingen.php
@@ -7,44 +7,46 @@
     <link rel="stylesheet" href="../css/nav.css?80970">
     <link rel="stylesheet" href="../css/meldingen.css?06978">
     <link rel="stylesheet" href="../css/shared.css">
-
 </head>
 <body>
     <?php include_once("../components/headerPages.inc.php"); ?>
 
+    <div class="container">
+        <div class="meldingen">
+            <h2>Meldingen</h2>
+        </div>
+        <div class="button-bar">
+            <h3>Vandaag</h3>
+            <div class="content button">
+                <p>Zitting in Brussel op woensdag 22 juni</p>
+            </div>
+            <div class="content button">
+                <p>Activiteit in Brussel georganisseerd door Unia!</p>
+            </div>
 
-    <div class="meldingen">
-        <h1>Meldingen</h1>
+            <h3 class="date">14 juni 2024</h3>
+            <div class="content button">
+                <p>Vrijwilligerswerk op maat! <a href="#">Klik hier</a> voor meer informatie</p>
+            </div>
+
+            <h3 class="date">13 juni 2024</h3>
+            <div class="content button">
+                <p>Zitting in Brussel op woensdag 28 juni</p>
+            </div>
+          
+            <div class="content button">
+                <p>NIEUW! Vrijwilligerswerk bij Myria</p>
+            </div>
+
+            <h3 class="date">12 juni 2024</h3>
+            <div class="content button">
+                <p>Zitting in Brussel op woensdag 15 juni</p>
+            </div>
+          
+            <div class="content button">
+                <p>NIEUW! Activiteit georganisseerd door NOOZO</p>
+            </div>
+        </div>
     </div>
-    <h3>Vandaag</h3>
-        <div class="content">
-            <p>Zitting in Brussel op woensdag 22 juni</p>
-        </div>
-        <div class="content">
-            <p>Activiteit in Brussel georganisseerd door Unia!</p>
-        </div>
-
-        <h3 class="date">14 juni 2024</h3>
-        <div class="content">
-            <p>Vrijwilligerswerk op maat! <a href="#">Klik hier</a> voor meer informatie</p>
-        </div>
-
-        <h3 class="date">13 juni 2024</h3>
-        <div class="content">
-            <p>Zitting in Brussel op woensdag 28 juni</p>
-        </div>
-      
-        <div class="content">
-            <p>NIEUW! Vrijwilligerswerk bij Myria</p>
-        </div>
-
-        <h3 class="date">12 juni 2024</h3>
-        <div class="content">
-            <p>Zitting in Brussel op woensdag 15 juni</p>
-        </div>
-      
-        <div class="content">
-            <p>NIEUW! Activiteit georganisseerd door NOOZO</p>
-        </div>
 </body>
 </html>
