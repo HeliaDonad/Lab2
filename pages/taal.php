d2974a12e0edb226fd30dbb59424b6dcfd969b86
HeliaDonad <r0936750@student.thomasmore.be>
2024-06-16 11:51:12 +0200

    ongeveer, nog aanpassen

--- pages/taal.php
+++ pages/taal.php
@@ -7,61 +7,61 @@
     <link rel="stylesheet" href="../css/nav.css?80970">
     <link rel="stylesheet" href="../css/shared.css">
     <link rel="stylesheet" href="../css/taal.css?02566">
-
-
 </head>
 <body>
     <?php include_once("../components/headerPages.inc.php"); ?>
 
-    <div class="meldingen">
-        <h1>Taal</h1>
-    </div>
-    
-    <div class="content">
-        <img src="../images/talen/engels.svg" alt="">
-        <div class="text-toggle">
-            <p>English</p>
-            <label class="switch">
-                <input type="checkbox" class="toggle-input">
-                <span class="slider round"></span>
-            </label>
+    <div class="container">
+        <div class="taal">
+            <h2>Taal</h2>
         </div>
-    </div>
-    
-    <div class="content">
-        <img src="../images/talen/nederlands.svg" alt="">
-        <div class="text-toggle">
-            <p>Nederlands</p>
-            <label class="switch">
-                <input type="checkbox" class="toggle-input" checked>
-                <span class="slider round"></span>
-            </label>
-        </div>
-    </div>
-    
-    <div class="content">
-        <img src="../images/talen/frans.svg" alt="">
-        <div class="text-toggle">
-            <p>Français</p>
-            <label class="switch">
-                <input type="checkbox" class="toggle-input">
-                <span class="slider round"></span>
-            </label>
-        </div>
-    </div>
-    
-    <div class="content">
-        <img src="../images/talen/duits.svg" alt="">
-        <div class="text-toggle">
-            <p>Deutsch</p>
-            <label class="switch">
-                <input type="checkbox" class="toggle-input">
-                <span class="slider round"></span>
-            </label>
+        <div class="button-bar">
+            <div class="content button">
+                <img src="../images/talen/engels.svg" alt="">
+                <div class="text-toggle">
+                    <p>English</p>
+                    <label class="switch">
+                        <input type="checkbox" class="toggle-input">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
+            
+            <div class="content button">
+                <img src="../images/talen/nederlands.svg" alt="">
+                <div class="text-toggle">
+                    <p>Nederlands</p>
+                    <label class="switch">
+                        <input type="checkbox" class="toggle-input" checked>
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
+            
+            <div class="content button">
+                <img src="../images/talen/frans.svg" alt="">
+                <div class="text-toggle">
+                    <p>Français</p>
+                    <label class="switch">
+                        <input type="checkbox" class="toggle-input">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
+            
+            <div class="content button">
+                <img src="../images/talen/duits.svg" alt="">
+                <div class="text-toggle">
+                    <p>Deutsch</p>
+                    <label class="switch">
+                        <input type="checkbox" class="toggle-input">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
         </div>
     </div>
 
-
-<script src="../js/toggle.js"></script>
+    <script src="../js/toggle.js"></script>
 </body>
 </html>
